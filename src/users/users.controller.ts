c9e9d8c727c0562b80311598538c6a0da16270fe
Andre Oliveira <andre.pbo@gmail.com>
2024-08-25 21:27:30 +0100

    feat(microservice): configure users client module with RabbitMQ

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(
+        private readonly usersService: UsersService,
+        @Inject('USER_SERVICE') private readonly client: ClientProxy,
+    ) { }
 
     @Get()
     async all() {
@@ -16,7 +20,9 @@ export class UsersController {
         @Body('email') email: string,
         @Body('password') password: string,
     ) {
-        return this.usersService.create({ username, email, password });
+        const user = await this.usersService.create({ username, email, password });
+        this.client.emit('user_created', user);
+        return user;
     }
 
     @Get(':id')
@@ -31,11 +37,15 @@ export class UsersController {
         @Body('email') email: string,
         @Body('password') password: string,
     ) {
-        return this.usersService.update(id, { username, email, password });
+        await this.usersService.update(id, { username, email, password });
+        const user = await this.usersService.get(id);
+        this.client.emit('user_updated', user);
+        return user;
     }
 
     @Delete(':id')
     async delete(@Param('id') id: number) {
-        return this.usersService.delete(id);
+        await this.usersService.delete(id);
+        this.client.emit('user_deleted', id);
     }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,26 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
