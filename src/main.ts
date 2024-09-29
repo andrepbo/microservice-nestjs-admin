ac3d0aae416ba942913bb6fb51479f846259c0cf
Andre Oliveira <andre.pbo@gmail.com>
2024-09-29 19:36:44 +0100

    include comments

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Create the HTTP API server
   const app = await NestFactory.create(AppModule);
+
+  // Set the global prefix for the API
   app.setGlobalPrefix('api');
+
+  // Start both the HTTP server
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
